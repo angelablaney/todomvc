6a6dae533808504e581f8df09a4616d3ab67190e
Angie Blaney <angela.blaney@jdpa.com>
2021-11-29 10:38:30 -0500

    Fixed lesson 9 pt 1. Had to re-add json creators/object mapping/data type transformer logic (since cucumber no longer auto transforms data) and trim the white spaces from 'buy some milk, Walk the dog' because the system was reading it as one line, not separate strings.

--- src/test/java/todomvc/AddNewTasksStepDefinitions.java
+++ src/test/java/todomvc/AddNewTasksStepDefinitions.java
@@ -1,14 +1,62 @@
 package todomvc;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import io.cucumber.java.DefaultDataTableCellTransformer;
+import io.cucumber.java.DefaultDataTableEntryTransformer;
+import io.cucumber.java.DefaultParameterTransformer;
+import io.cucumber.java.ParameterType;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
 import todomvc.steps.TodoListUser;
 
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
 
 public class AddNewTasksStepDefinitions {
+    // register data type handler for converting lists
+    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    @DefaultParameterTransformer
+    @DefaultDataTableEntryTransformer
+    @DefaultDataTableCellTransformer
+    public Object defaultTransformer(Object fromValue, Type toValueType) {
+        JavaType javaType = objectMapper.constructType(toValueType);
+
+        if(fromValue instanceof String && ((String) fromValue).contains(","))
+        {
+            // determine it's a list of strings
+            try{
+                String[] splitArray = ((String) fromValue).split(",");
+                JsonArray jsonArray = new JsonArray();
+                for(String splitString : splitArray)
+                {
+                    jsonArray.add(splitString.trim());
+                }
+                return objectMapper.readValue(jsonArray.toString(), TypeFactory.defaultInstance().constructCollectionType(List.class, String.class));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return objectMapper.convertValue(fromValue, javaType);
+    }
+//
+//    @ParameterType("(?:.+;)+.+")
+//    public List<String> stringList(String raw) {
+//        String[] values = raw.split(";");
+//        return Arrays.asList(values);
+//    }
 
     @Steps
     TodoListUser todoUser;
--- src/test/java/todomvc/ui/TodoList.java
+++ src/test/java/todomvc/ui/TodoList.java
@@ -6,6 +6,7 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.Keys;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
